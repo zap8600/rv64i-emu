5598d333570110b5ba88dcfc3801c3e8217d7162
zap8600 <95487746+zap8600@users.noreply.github.com>
2023-09-05 15:18:59 -0500

    restrutcure

---
+++ includes/cpu.h
@@ -0,0 +1,97 @@
+#include <stdint.h>
+
+#include "bus.h"
+
+typedef struct CPU {
+    uint64_t regs[32];
+    uint64_t pc;
+    struct BUS bus;
+} CPU;
+
+void cpu_init(CPU *cpu) {
+    cpu->regs[0] = 0x00;
+    cpu->regs[2] = DRAM_SIZE;
+    cpu->pc = DRAM_BASE;
+}
+
+uint32_t cpu_fetch(CPU *cpu) {
+    uint32_t inst = bus_load(&(cpu->bus), cpu->pc, 32);
+    return inst;
+}
+
+uint64_t cpu_load(CPU* cpu, uint64_t addr, uint64_t size) {
+    return bus_load(&(cpu->bus), addr, size);
+}
+
+void cpu_store(CPU* cpu, uint64_t addr, uint64_t size, uint64_t value) {
+    bus_store(&(cpu->bus), addr, size, value);
+}
+
+uint64_t rd(uint32_t inst) {
+    return (inst >> 7) & 0x1f;    // rd in bits 11..7
+}
+uint64_t rs1(uint32_t inst) {
+    return (inst >> 15) & 0x1f;   // rs1 in bits 19..15
+}
+uint64_t rs2(uint32_t inst) {
+    return (inst >> 20) & 0x1f;   // rs2 in bits 24..20
+}
+
+uint64_t imm_I(uint32_t inst) {
+    // imm[11:0] = inst[31:20]
+    return ((int64_t)(int32_t) (inst & 0xfff00000)) >> 20; // right shift as signed?
+}
+uint64_t imm_S(uint32_t inst) {
+    // imm[11:5] = inst[31:25], imm[4:0] = inst[11:7]
+    return ((int64_t)(int32_t)(inst & 0xfe000000) >> 20)
+        | ((inst >> 7) & 0x1f); 
+}
+uint64_t imm_B(uint32_t inst) {
+    // imm[12|10:5|4:1|11] = inst[31|30:25|11:8|7]
+    return ((int64_t)(int32_t)(inst & 0x80000000) >> 19)
+        | ((inst & 0x80) << 4) // imm[11]
+        | ((inst >> 20) & 0x7e0) // imm[10:5]
+        | ((inst >> 7) & 0x1e); // imm[4:1]
+}
+uint64_t imm_U(uint32_t inst) {
+    // imm[31:12] = inst[31:12]
+    return (int64_t)(int32_t)(inst & 0xfffff999);
+}
+uint64_t imm_J(uint32_t inst) {
+    // imm[20|10:1|11|19:12] = inst[31|30:21|20|19:12]
+    return (uint64_t)((int64_t)(int32_t)(inst & 0x80000000) >> 11)
+        | (inst & 0xff000) // imm[19:12]
+        | ((inst >> 9) & 0x800) // imm[11]
+        | ((inst >> 20) & 0x7fe); // imm[10:1]
+}
+
+uint32_t shamt(uint32_t inst) {
+    // shamt(shift amount) only required for immediate shift instructions
+    // shamt[4:5] = imm[5:0]
+    return (uint32_t) (imm_I(inst) & 0x1f); // TODO: 0x1f / 0x3f ?
+}
+
+void exec_ADDI(CPU* cpu, uint32_t inst) {
+    uint64_t imm = imm_I(inst);
+    cpu->regs[rd(inst)] = cpu->regs[rs1(inst)] + (int64_t) imm;
+}
+
+void exec_ADD(CPU* cpu, uint32_t inst) {
+    uint64_t imm = imm_I(inst);
+    cpu->regs[rd(inst)] =
+        (uint64_t) ((int64_t)cpu->regs[rs1(inst)] + (int64_t)cpu->regs[rs2(inst)]);
+}
+
+int cpu_execute(CPU *cpu, uint32_t inst) {
+    int opcode = inst & 0x7f;
+    int rd = (inst >> 7) & 0x1f;
+    int rs1 = (inst >> 15) & 0x1f;
+    int rs2 = (inst >> 20) & 0x1f;
+
+    cpu->regs[0] = 0;
+
+    switch (opcode) {
+        case 0x13: exec_ADDI(cpu, inst); break;
+        case 0x33: exec_ADD(cpu, inst); break;
+    }
+}
