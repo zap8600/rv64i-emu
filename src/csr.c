d7841ac9ecb8911d2d6cff0fa913b4de4351a5e9
zap8600 <95487746+zap8600@users.noreply.github.com>
2023-09-20 09:53:46 -0500

    debug?

--- src/csr.c
+++ src/csr.c
@@ -5,6 +5,9 @@ uint64_t csr_read(CPU* cpu, uint64_t csr) {
     //return (uint64_t)(uint32_t)cpu->csr[csr];
     if (csr == SIE) {
         return ((uint64_t)(uint32_t)cpu->csr[MIE] & (uint64_t)(uint32_t)cpu->csr[MIDELEG]);
+    } else if (csr == MEPC) {
+        printf("mepc: %#-13.2lx", cpu->csr[MEPC]);
+        return (uint64_t)(uint32_t)cpu->csr[csr];
     } else {
         return (uint64_t)(uint32_t)cpu->csr[csr];
     }
@@ -14,6 +17,9 @@ void csr_write(CPU* cpu, uint64_t csr, uint64_t value) {
     //cpu->csr[csr] = value;
     if (csr == SIE) {
         cpu->csr[MIE] = (cpu->csr[MIE] & !(cpu->csr[MIDELEG])) | (value & cpu->csr[MIDELEG]);
+    } else if (csr == MEPC) {
+        printf("mepc: %#-13.2lx", cpu->csr[MEPC]);
+        cpu->csr[csr] = value;
     } else {
         cpu->csr[csr] = value;
     }
