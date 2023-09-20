a0e214a9700be8a402b5292d852ec242f39ff021
zap8600 <95487746+zap8600@users.noreply.github.com>
2023-09-20 09:26:29 -0500

    fix?

--- src/csr.c
+++ src/csr.c
@@ -2,9 +2,19 @@
 #include <stdint.h>
 
 uint64_t csr_read(CPU* cpu, uint64_t csr) {
-    return (uint64_t)(uint32_t)cpu->csr[csr];
+    //return (uint64_t)(uint32_t)cpu->csr[csr];
+    if (addr == SIE) {
+        return ((uint64_t)(uint32_t)cpu->csr[MIE] & (uint64_t)(uint32_t)cpu->csr[MIDLEG]);
+    } else {
+        return (uint64_t)(uint32_t)cpu->csr[csr];
+    }
 }
 
 void csr_write(CPU* cpu, uint64_t csr, uint64_t value) {
-    cpu->csr[csr] = value;
+    //cpu->csr[csr] = value;
+    if (addr == SIE) {
+        cpu->csr[MIE] = (cpu->csr[MIE] & !(cpu->csr[MIDLEG])) | (value & cpu->csr[MIDLEG]);
+    } else {
+        cpu->csr[csr] = value;
+    }
 }
