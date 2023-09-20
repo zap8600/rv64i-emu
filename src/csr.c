6e958709b6edcc3590a488c6451a87ed56062663
zap8600 <95487746+zap8600@users.noreply.github.com>
2023-09-20 09:27:45 -0500

    fix?

--- src/csr.c
+++ src/csr.c
@@ -4,7 +4,7 @@
 uint64_t csr_read(CPU* cpu, uint64_t csr) {
     //return (uint64_t)(uint32_t)cpu->csr[csr];
     if (csr == SIE) {
-        return ((uint64_t)(uint32_t)cpu->csr[MIE] & (uint64_t)(uint32_t)cpu->csr[MIDLEG]);
+        return ((uint64_t)(uint32_t)cpu->csr[MIE] & (uint64_t)(uint32_t)cpu->csr[MIDELEG]);
     } else {
         return (uint64_t)(uint32_t)cpu->csr[csr];
     }
@@ -13,7 +13,7 @@ uint64_t csr_read(CPU* cpu, uint64_t csr) {
 void csr_write(CPU* cpu, uint64_t csr, uint64_t value) {
     //cpu->csr[csr] = value;
     if (csr == SIE) {
-        cpu->csr[MIE] = (cpu->csr[MIE] & !(cpu->csr[MIDLEG])) | (value & cpu->csr[MIDLEG]);
+        cpu->csr[MIE] = (cpu->csr[MIE] & !(cpu->csr[MIDELEG])) | (value & cpu->csr[MIDELEG]);
     } else {
         cpu->csr[csr] = value;
     }
