dfe66b8f5740f42953a8122b9dc936b63bf5d4bf
zap8600 <95487746+zap8600@users.noreply.github.com>
2023-09-20 11:47:50 -0500

    debug

--- main.c
+++ main.c
@@ -116,7 +116,7 @@ int main(int argc, char* argv[]) {
             printf("paging off!\n");
         }
         */
-        //dump_registers(&cpu);
+        dump_registers(&cpu);
         dump_csr(&cpu);
     }
     dump_registers(&cpu);
