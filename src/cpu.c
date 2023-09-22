e1ad197e90c4ddf3408e0a3d4012b504b41b678c
zap8600 <95487746+zap8600@users.noreply.github.com>
2023-09-22 11:47:29 -0500

    debug

--- src/cpu.c
+++ src/cpu.c
@@ -174,6 +174,9 @@ uint64_t cpu_translate(CPU* cpu, uint64_t addr, AccessType access_type) {
 }
 
 uint64_t cpu_load(CPU* cpu, uint64_t addr, uint64_t size) {
+    if(cpu->enable_paging) {
+        printf("paging!");
+    }
     uint64_t p_addr = cpu_translate(cpu, addr, Load);
     //printf("load=%lx %lx %lx\n", p_addr, size, bus_load(&(cpu->bus), p_addr, size));
     return bus_load(&(cpu->bus), p_addr, size);
